98f75a92e3f8a083d29742788db6e5f57873de87
arshyiva <127118961+arshyiva@users.noreply.github.com>
2024-01-15 15:26:37 +0700

    Add files via upload
    ArshyivaUlyaZain - 3122101253 - UAS

---
+++ android/app/src/main/kotlin/com/example/santri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.santri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://yqqrgrhtzbuxvpsvtatz.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxcXJncmh0emJ1eHZwc3Z0YXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUzMDUwMjksImV4cCI6MjAyMDg4MTAyOX0.T0w_mS0EScn--FGlwgt--4715GzKZY4Uf04lKfA8wWY',
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Countries',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _future = Supabase.instance.client.from('countries').select();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final countries = snapshot.data!;
+          return ListView.builder(
+            itemCount: countries.length,
+            itemBuilder: ((context, index) {
+              final country = countries[index];
+              return ListTile(
+                leading: Icon(
+                  Icons.flag_circle_rounded,
+                  color: Colors.red,
+                ),
+                // trailing: Icon(Icons.flag_circle_outlined, color: Colors.amber),
+                title: Text(
+                  country['name'],
+                  style: TextStyle(color: Colors.blue),
+                ),
+                subtitle: Text(country['benua']),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
